3c321096da0a5c8126c2edfb96d2a0a9863165d6
darunbkarthik <126142984+darunbkarthik@users.noreply.github.com>
2025-08-17 00:49:30 +0530

    Add files via upload
    Day 1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "John Doe";
+		int age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("Name: "+name+"\n"+"Age: "+age+"\n"+"Salary: "+salary+"\n"+"Gender: "+gender+"\n"+"Accept Terms: "+acceptTerms+
+				"\n"+"Rating: "+rating+"\n"+"Mobile Number: "+mobileNumber+"\n"+"Pincode: "+pinCode+"\n"+"Lucky Number: "+luckyNumber);
+	}
+
+}
